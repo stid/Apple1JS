81f7c3e723019a023c26c89667985326e368e3d6
Stefano Furiosi <stid72@gmail.com>
2021-03-16 21:52:19 -0700

    Revert "Revert "Update babel.config.js""
    This reverts commit 772fa77bb1ef12b5227ff655418ea9f3235048e4.

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,9 @@ module.exports = function(api) {
     api.cache(true);
 
     const presets = [
-        ['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3, targets: { chrome: '76', node: '12' } }],
+        ['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3, targets: { chrome: '89', node: '14' } }],
         '@babel/typescript',
-        '@babel/preset-react',
-        { runtime: 'automatic' },
+        ['@babel/preset-react', { runtime: 'automatic' }],
     ];
 
     const plugins = [
